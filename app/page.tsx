80dd06cf5ef07c64536482da448e03d321a374dc
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2026-05-21 22:19:22 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center p-10">
-      <h1 className="h1">Project L.I.B.R.A.R.Y.</h1>
+      <h1 className="h1">Project LIBRARY</h1>
       <h2 className="h2">
         Literacy Initiative for Boosting Reading Access and Resources Year-round
       </h2>
