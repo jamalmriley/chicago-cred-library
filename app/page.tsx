3740e3b44348905ae10cca5927c1fe244ca60051
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2026-06-30 00:22:14 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default async function HomePage() {
               </Link>
             </Button>
           )}
-          <Button asChild className="molde-button bg-white text-black">
+          <Button variant="outline" asChild className="molde-button">
             <Link href="https://github.com/jamalmriley/project-library">
               <Code />
               View project
