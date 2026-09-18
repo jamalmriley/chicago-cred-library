219c49bcf025013d2bac4cca4893b6fe5517cb8b
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2026-09-18 12:00:55 -0500

    Update route.ts

--- app/api/reminder/route.ts
+++ app/api/reminder/route.ts
@@ -40,7 +40,7 @@ export async function GET() {
       const unreturnedBooks = participant.checkout_history.filter((item) => {
         if (!item || !item.due_date) return false;
 
-        const dueDate = item.due_date;
+        const dueDate = new Date(String(item.due_date));
         const daysUntilDue = Math.ceil(
           (dueDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24), // ms * sec * min * hour
         );
